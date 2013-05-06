6fd6c58eb5abbc268751c5e7abbc0e85d1eabe28
Thomas Roten <thomas@roten.us>
2013-05-06 19:50:11 +0800

    Adds logging to build_string. Fixes #10

--- zhon/__init__.py
+++ zhon/__init__.py
@@ -2,5 +2,13 @@
 
 __version__ = '0.1.1'
 
+import logging
+
 from . import unicode
 from . import cedict
+
+logger = logging.getLogger(__name__)
+try:
+    logger.addHandler(logging.NullHandler())
+except AttributeError:  # catch error for Python 2.6
+    pass
